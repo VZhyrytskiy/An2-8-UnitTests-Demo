e712421e031378d158e48f6be464e63922106e27
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-05-26 11:29:22 +0300

    override component providers

--- src/app/components/09-override-component-providers/msg-list/msg-list.component.spec.ts
+++ src/app/components/09-override-component-providers/msg-list/msg-list.component.spec.ts
@@ -1,25 +1,52 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MsgListComponent } from './msg-list.component';
+import { MsgListService } from './msg-list.service';
+import { By } from '@angular/platform-browser';
+
+// stub MsgListServiceSpy for test purposes
+const msgListServiceSpy = {
+  getMessges() {
+    return [
+      { 'id': 1, 'msg': 'Hi, TestUser1' },
+      { 'id': 2, 'msg': 'Hi, TestUser2' }
+    ];
+  }
+};
 
 describe('MsgListComponent', () => {
   let component: MsgListComponent;
   let fixture: ComponentFixture<MsgListComponent>;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MsgListComponent ]
-    })
-    .compileComponents();
+    TestBed
+      // Configure module
+      .configureTestingModule({
+        declarations: [MsgListComponent]
+      })
+
+      // Override component's own provider
+      .overrideComponent(MsgListComponent, {
+        set: {
+          providers: [
+            { provide: MsgListService, useValue: msgListServiceSpy }
+          ]
+        }
+      })
+      // Compile templates and styles
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MsgListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    el = fixture.debugElement.query(By.css('.msg')).nativeElement;
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display Hi, TestUser1', () => {
+    expect(el.textContent).toBe('Hi, TestUser1');
   });
 });
