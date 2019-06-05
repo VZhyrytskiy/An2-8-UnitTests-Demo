80cb2f231ad3a9d607f34bd5325bc9926c909dba
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2019-06-05 13:05:25 +0300

    minor fix

--- src/app/01-isolated-tests/components/02-component-w-io/io.component.spec.ts
+++ src/app/01-isolated-tests/components/02-component-w-io/io.component.spec.ts
@@ -1,12 +1,15 @@
 import { IoComponent, IO } from './io.component';
 
 describe('IoComponent', () => {
-  it('raises the pass event when clicked', () => {
+  it('raises the pass event when clicked', (done: DoneFn) => {
     const comp = new IoComponent();
     const data: IO = { id: 42, value: 'Test' };
     comp.data = data;
 
-    comp.pass.subscribe(d => expect(d).toBe(data));
+    comp.pass.subscribe(d => {
+      expect(d).toBe(data);
+      done();
+    });
     comp.onClick();
   });
 });
