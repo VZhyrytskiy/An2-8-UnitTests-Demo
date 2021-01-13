6eb9f15fbf7ad273ed6714ff881c1f2096f74673
VZhyrytskiy <Vitaliy_Zhyrytskiy@epam.com>
2021-01-13 19:10:54 +0200

    upd: update some project files

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-unit-tests!');
+  it('should display welcome message', async () => {
+    await page.navigateTo();
+    expect(await page.getTitleText()).toEqual('an2 app is running!');
   });
 
   afterEach(async () => {
